3dc765931ead447aeb375903f238bc4d25ac4bcd
Hoaveth <hoaveth.gaming@gmail.com>
2023-08-01 17:08:35 +0800

    Added services and utility folders

---
+++ src/services/UserService/service.ts
@@ -0,0 +1,63 @@
+import axios from 'axios';
+
+import { IUserData } from '.';
+
+const axiosInstance = axios.create({
+  baseURL: 'https://jsonplaceholder.typicode.com/posts',
+  timeout: 60000,
+});
+
+export const UserService = {
+  getAll: async function () {
+    try {
+      const { data } = await axiosInstance.request({
+        method: 'GET',
+        url: `?_limit=10`,
+      });
+      return data;
+    } catch (error) {
+      console.error(error);
+      throw error;
+    }
+  },
+  getById: async function (userId: number) {
+    try {
+      const { data } = await axiosInstance.request({
+        method: 'GET',
+        url: `/${userId}`,
+      });
+      return data;
+    } catch (error) {
+      console.error(error);
+      throw error;
+    }
+  },
+  create: async function (user: IUserData) {
+    try {
+      const response = await axiosInstance.request({
+        method: 'POST',
+        url: `/api/v1/users`,
+        data: user,
+      });
+      return response;
+    } catch (error) {
+      console.error(error);
+      throw error;
+    }
+  },
+  update: async function (userId: number, user: IUserData) {
+    try {
+      const response = await axiosInstance.request({
+        method: 'PUT',
+        url: `/api/v1/users/${userId}`,
+        data: user,
+      });
+      return response;
+    } catch (error) {
+      console.error(error);
+      throw error;
+    }
+  },
+};
+
+export default UserService;
